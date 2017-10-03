cb242f4d0ac602fe0eda7e5444db9da2b0fd6085
Liam Barstad <liambarstad@gmail.com>
2017-10-03 14:34:10 -0600

    add search params for invoice

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -9,23 +9,17 @@ class Api::V1::InvoicesController < ApplicationController
   end
 
   def find
-    render json: get_query_results[0]
+    render json: Invoice.find_by(search_params)
   end
 
   def find_all
-    render json: get_query_results
+    render json: Invoice.find_all(search_params)
   end
 
   private
 
-  def get_query_results
-    results = []
-    params.each_pair do |param_name, param_value|
-      if Invoice.has_attribute?(param_name)
-        results << Invoice.where(param_name.to_sym => param_value).to_a
-      end
-    end
-    results.flatten
+  def search_params
+    params.permit(:id, :status, :customer, :merchant)
   end
 
 end
