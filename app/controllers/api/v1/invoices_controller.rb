5f6793ec8c0d508b824924786b839a87903c62b2
Liam Barstad <liambarstad@gmail.com>
2017-10-03 14:23:46 -0600

    add find tests, find route, preliminary find method

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -8,4 +8,24 @@ class Api::V1::InvoicesController < ApplicationController
     render json: Invoice.find(params[:id])
   end
 
+  def find
+    render json: get_query_results[0]
+  end
+
+  def find_all
+    render json: get_query_results
+  end
+
+  private
+
+  def get_query_results
+    results = []
+    params.each_pair do |param_name, param_value|
+      if Invoice.has_attribute?(param_name)
+        results << Invoice.where(param_name.to_sym => param_value).to_a
+      end
+    end
+    results.flatten
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get "/invoices/find", to: "invoices#find"
+      get "/invoices/find_all", to: "invoices#find_all"
       resources :invoices, only: [:index, :show]
     end
   end
