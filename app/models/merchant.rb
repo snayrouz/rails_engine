f67f52110f2dfccf4a783ce0244efa6e7a4f30f8
snayrouz <samuel.nayrouz@gmail.com>
2017-10-03 23:12:14 -0600

    Add AR methods to merchants for BI

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,12 +5,11 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def total_revenue
-    invoices.
+    invoices.joins(:transactions, :invoice_items).merge(Transaction.successful).sum('invoice_items.unit_price * invoice_items.quantity')
   end
 
   def total_revenue_by_date(date)
-    invoices.
+    invoices..joins(:transactions, :invoice_items).merge(Transaction.successful).where(invoices: {created_at: date}).sum('invoice_items.unit_price * invoice_items.quantity')
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,15 @@ Rails.application.routes.draw do
         get '/:id/revenue', to: 'revenue#show'
       end
 
+      namespace :transactions do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+        get '/:id/invoice', to: 'invoices#index'
+      end
+
       resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
     end
   end
 end
