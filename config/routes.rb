eb33fe6a88535e1bf0c020b585aa540f3f64dcca
snayrouz <samuel.nayrouz@gmail.com>
2017-10-04 13:17:13 -0600

    Added controllers for models and custom rest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,54 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1 do
+      namespace :items do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/merchant', to: 'merchants#show'
+        get '/:id/best_day', to: 'best_day#show'
+        get '/most_items', to: 'most_items#index'
+      end
 
-      namespace 'merchants' do
-        get "find_all", to: "search#index"
-        get "find", to: "search#show"
-        get "random", to: "random#show"
+      namespace :invoices do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+        get '/:id/transactions', to: 'transactions#index'
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/items', to: 'items#index'
+        get '/:id/merchant', to: 'merchants#show'
+        get '/:id/customer', to: 'customers#show'
+      end
+
+      namespace :invoice_items do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+        get '/:id/invoice', to: 'invoices#show'
+        get '/:id/item', to: 'items#show'
+      end
+
+      namespace :customers do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+        get '/:id/transactions', to: 'transactions#index'
+        get '/:id/invoices', to: 'invoices#index'
+        get '/:id/favorite_merchant', to: 'favorite_merchant#show'
+      end
+
+      namespace :merchants do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+        get '/most_items', to: 'most_items#index'
+        get "/:id/items", to: 'items#index'
+        get "/:id/invoices", to: 'invoices#index'
         get '/:id/revenue', to: 'revenue#show'
+        get '/:id/favorite_customer', to: 'favorite_customer#show'
+        get '/most_revenue', to: 'most_revenue#index'
       end
 
       namespace :transactions do
@@ -18,6 +59,10 @@ Rails.application.routes.draw do
       end
 
       resources :merchants, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+      resources :customers, only: [:index, :show]
       resources :transactions, only: [:index, :show]
     end
   end
