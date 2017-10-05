7f33502a3729577949d5b23921668d8ec2425669
snayrouz <samuel.nayrouz@gmail.com>
2017-10-05 15:25:15 -0600

    Change namespace apis

--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::MostItemsContoller < ApplicationController
+class Api::V1::Merchants::MostItemsContoller < ApplicationController
   def index
     render json: Merchant.most_items(params[:quantity])
   end
--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,6 +1,5 @@
-class Api::V1::MostRevenueController < ApplicationController
+class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
-    binding.pry
     render json: Merchant.top_by_revenue(params[:quantity])
   end
 end
