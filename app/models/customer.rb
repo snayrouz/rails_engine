3c3b2a12b8fff763cd5324b1d832b02dc2c7e018
snayrouz <samuel.nayrouz@gmail.com>
2017-10-05 10:45:23 -0600

    Add bi for items and customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,15 @@
 class Customer < ApplicationRecord
-  validates_presence_of :first_name, :last_name
+  has_many :invoices
+  has_many :transactions, through: :invoices
+  validates :first_name, :last_name, presence: true
 
   def favorite_merchant
-
+    invoices
+    .select('merchants.*, count(invoices.id) AS customer_transactions')
+    .joins(:transactions, :merchant)
+    .merge(Transaction.successful)
+    .group('merchants.id')
+    .order('customer_transactions DESC')
+    .first
   end
-
-  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,22 @@ class Item < ApplicationRecord
   before_save :set_unit_price
 
   def best_day
+    invoices
+    .select('invoices.*, sum(invoice_items.quantity) AS purchases')
+    .joins(:transactions, :invoice_items)
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('purchases DESC, invoices.created_at DESC')
+    .first
   end
 
   def self.most_items(quantity = nil)
+    select('items.*, sum(invoice_items.quantity) AS num_sold')
+    .joins(:invoice_items, invoices: [:transactions])
+    .merge(Transaction.successful)
+    .group(:id)
+    .order('num_sold DESC')
+    .limit(quantity)
   end
 
   private
