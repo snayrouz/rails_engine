cf0246afd81f639c552df50c2ac6ba2fad55dc21
Liam Barstad <liambarstad@gmail.com>
2017-10-05 20:59:00 -0600

    turn merchant individual revenue to string

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::Merchants::RevenueController < ApplicationController
   def show
     merchant = Merchant.find(params[:id])
     if params[:date]
-      render json: { "revenue" => merchant.total_revenue_by_date(params[:date]) }
+      render json: { "revenue" => merchant.total_revenue_by_date(params[:date]).to_s }
     else
-      render json: { "revenue" => merchant.total_revenue }
+      render json: { "revenue" => merchant.total_revenue.to_s }
     end
   end
 
